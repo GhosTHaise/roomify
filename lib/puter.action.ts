7f8062e5eeccb980b97167b02cf695f8e01c0d14
GhosTHaise <ghostrex2@gmail.com>
2026-02-23 20:17:19 +0300

    feat : host and upload iamges

--- lib/puter.action.ts
+++ lib/puter.action.ts
@@ -1,4 +1,6 @@
 import puter from "@heyputer/puter.js";
+import { getOrCreateHostingConfig, uploadImageToHosting } from "./puter.hosting";
+import { isHostedUrl } from "./utils";
 
 export const signIn = async () => await puter.auth.signIn();
 export const signOut = async () => puter.auth.signOut();
@@ -7,6 +9,57 @@ export const getCurrentUser = async () => {
     try {
         return await puter.auth.getUser();
     } catch (error) {
+        return null;
+    }
+}
+
+export const createProject = async ({ item }: CreateProjectParams): Promise<DesignItem | null | undefined> => {
+    const projectId = item.id;
+
+    const hosting = await getOrCreateHostingConfig();
+
+    const hostedSource = projectId ? await uploadImageToHosting(
+        {
+            hosting,
+            url: item.sourceImage,
+            projectId,
+            label: "source"
+        }
+    ) : null
+
+    const hostedRender = projectId && item.renderedImage ? await uploadImageToHosting({
+        hosting,
+        url : item.renderedImage,
+        projectId,
+        label : "rendered"
+    }) : null;
+
+    const resolvedSource = hostedSource?.url || (isHostedUrl(item.sourceImage) ? item.sourceImage : "");
+    if(!resolvedSource) {
+        console.warn("Failed to host source image, skipping save.")
+    }
+
+    const resolvedRender = hostedRender?.url ? hostedRender.url : item.renderedImage && isHostedUrl(item.renderedImage) ? item.renderedImage : undefined;
+
+    const {
+        sourcePath : _sourcePath,
+        renderedPath : _renderPath,
+        publicPath : _publicPath,
+        ...rest
+    } = item;
+
+    const payload = {
+        ...rest,
+        sourceImage : resolvedSource,
+        renderedImage : resolvedRender,
+    }
+    
+    try {
+        // call the Puter worker to store project in kv
+
+        return payload;
+    } catch (error) {
+        console.error("Failed to save project", error);
         return null;
     }
 }
